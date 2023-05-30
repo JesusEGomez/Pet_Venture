8cd989aa9c6bccea45bda917a482350df19784ea
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-05-30 17:33:16 -0300

    Añadiendo tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import "./styles/tailwind.css"
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editingg&nbsp;
+          Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
