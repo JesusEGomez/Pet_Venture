9b425a9298b4d53f5dd9f4d0a6eddd6af7e0ea08
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-05-31 08:39:51 -0300

    terminando de configurar tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import "./styles/tailwind.css"
 export default function Home() {
   return (
     <main className={styles.main}>
